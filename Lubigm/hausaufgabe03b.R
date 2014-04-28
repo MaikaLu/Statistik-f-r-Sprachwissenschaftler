4666eb109579b1d7af10fd98d7c672909a40a184
Lubigm <Lubigm@students.uni-marburg.de>
2014-04-28 20:49:53 +0200

    Hausaufgabe b: Name und Datum geändert

--- Lubigm/hausaufgabe03b.R
+++ Lubigm/hausaufgabe03b.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 03
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-04-23
+# Maika Lubig <Lubigm@students.uni-marburg.de>
+# 2014-04-28
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
